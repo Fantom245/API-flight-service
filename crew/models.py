cfe361610372fb8ee8b5316a0e29a0cfd5a005a4
unknown <gyrkalomas@gmail.com>
2025-06-03 01:11:09 +0300

    create Crew(CustomUser) model and remaming it in settings.py

--- crew/models.py
+++ crew/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Crew(AbstractUser):
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def __str__(self):
+        return self.full_name
