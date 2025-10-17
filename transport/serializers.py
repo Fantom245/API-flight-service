70fe11fda25db8250e9729986682377f1cd90b88
unknown <gyrkalomas@gmail.com>
2025-10-17 03:22:14 +0300

    update serializers for transport app

--- transport/serializers.py
+++ transport/serializers.py
@@ -7,14 +7,20 @@ class AirplaneTypeListSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
         fields = ["id", "name", "manufacturer", "max_range_km", "max_speed_kmh"]
+        read_only_fields = ["id", "created_at", "updated_at"]
 
 
 class AirplaneListSerializer(serializers.ModelSerializer):
     number_of_seats = serializers.ReadOnlyField()
-    
+    airplane_type = serializers.SlugRelatedField(
+        slug_field = "name",
+        queryset = AirplaneType.objects.all()
+    )
+
     class Meta:
         model = Airplane
         fields = ["id", "name", "rows", "seats_in_row", "number_of_seats", "airplane_type", "status"]
+        read_only_fields = ["id", "created_at", "updated_at"]
 
 
 class AirplaneDetailSerializer(AirplaneListSerializer):
